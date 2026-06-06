142dcbf57333477b4bbc875d7496be27d68c3c5a
yashrohilla09 <yashrohilla0777@gmail.com>
2026-06-06 13:07:47 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,26 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDfR46sDM8DIQr9Fm7g8R2hh50lvikoPQo",
+  authDomain: "gju-resource-hub.firebaseapp.com",
+  projectId: "gju-resource-hub",
+  storageBucket: "gju-resource-hub.firebasestorage.app",
+  messagingSenderId: "1074524934130",
+  appId: "1:1074524934130:web:620e76a8baca499989a56b"
+};
+
+window.db = null;
+window.auth = null;
+window.storage = null;
+window.isFirebaseInitialized = false;
+
+try {
+    if (firebaseConfig.apiKey && firebaseConfig.apiKey.trim() !== "") {
+        firebase.initializeApp(firebaseConfig);
+        window.db = firebase.firestore();
+        window.auth = firebase.auth();
+        window.storage = firebase.storage();
+        window.isFirebaseInitialized = true;
+        console.log("Firebase Cloud Backend Initialized Successfully.");
+    }
+} catch (error) {
+    console.error(error);
+}
